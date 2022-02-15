252605005fc181cfab87c70840ff8c1b0d9937a7
Colum Ferry <cferry09@gmail.com>
2022-02-15 14:07:23 +0000

    initial commit

---
+++ apps/app1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ng-medium-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app1';
+}
