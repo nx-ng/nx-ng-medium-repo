19689ccbf81526d78b69a718b11e508d6b6a0503
Colum Ferry <cferry09@gmail.com>
2022-02-15 15:55:17 +0000

    chore(repo): add benchmarking scripts

--- apps/app0/src/app/app.component.spec.ts
+++ apps/app0/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Lib0Module } from '@nx-ng-medium/app0/lib0';
+import { Lib1Module } from '@nx-ng-medium/app0/lib1';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, Lib0Module, Lib1Module],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -20,14 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app0');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to app0!'
-    );
   });
 });
