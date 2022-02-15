bd439852af9281eac94e5914e4753bc58fb889dd
Colum Ferry <cferry09@gmail.com>
2022-02-15 16:34:31 +0000

    chore(repo): setup app structure

--- apps/app0-e2e/src/integration/app.spec.ts
+++ apps/app0-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('app0', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to app0!');
+    getGreeting().contains('Welcome app0');
   });
 });
--- apps/app0/src/app/app.component.spec.ts
+++ apps/app0/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Lib0Module } from '@nx-ng-medium/app0/lib0';
+import { Lib1Module } from '@nx-ng-medium/app0/lib1';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule, Lib0Module, Lib1Module],
+      declarations: [AppComponent, NxWelcomeComponent],
     }).compileComponents();
   });
 
@@ -20,14 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app0');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to app0!'
-    );
   });
 });
