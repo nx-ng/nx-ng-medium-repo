cf7def132a4dcf9b30f969dd859e0ee1a1a6b3c0
Colum Ferry <cferry09@gmail.com>
2022-02-17 14:02:26 +0000

    chore(repo): add current version checkout script

---
+++ tools/scripts/build-current-version.mjs
@@ -0,0 +1,79 @@
+import { execSync } from 'child_process';
+import { existsSync, readFileSync, rmSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import ora from 'ora';
+
+const PATH_TO_NX_LOCAL = join(process.cwd(), 'tmp/nx');
+
+function runWithConsoleSpinner(text, action) {
+  const spinner = ora({ text, color: 'cyan' });
+  spinner.start();
+  action();
+  spinner.succeed();
+}
+
+function tearDown() {
+  if (existsSync(PATH_TO_NX_LOCAL)) {
+    runWithConsoleSpinner('Removing existing Nx Repo', () => {
+      rmSync(PATH_TO_NX_LOCAL, { recursive: true, force: true });
+    });
+  }
+}
+
+function cloneNx() {
+  runWithConsoleSpinner('Cloning Nx into tmp/nx', () => {
+    execSync(`git clone https://github.com/nrwl/nx.git ${PATH_TO_NX_LOCAL}`, {
+      stdio: 'ignore',
+    });
+  });
+}
+
+function installNxDeps() {
+  runWithConsoleSpinner('Install Nx Dependencies', () => {
+    execSync(`yarn install`, { cwd: PATH_TO_NX_LOCAL, stdio: 'ignore' });
+  });
+}
+
+function buildNx() {
+  runWithConsoleSpinner('Build Nx', () => {
+    execSync(`yarn build`, { cwd: PATH_TO_NX_LOCAL, stdio: 'pipe' });
+  });
+}
+
+//update package.json to point to dist folders
+function updatePackageJsonPointToDist() {
+  runWithConsoleSpinner('Pointing to local Nx builds', () => {
+    const PKG_JSON_PATH = join(process.cwd(), 'package.json');
+    const pkgJson = JSON.parse(
+      readFileSync(PKG_JSON_PATH, { encoding: 'utf-8' })
+    );
+    const updatedPkgJson = {
+      ...pkgJson,
+      dependencies: {
+        ...pkgJson.dependencies,
+        '@nrwl/angular': 'file:./tmp/nx/build/packages/angular',
+      },
+      devDependencies: {
+        ...pkgJson.devDependencies,
+        '@nrwl/cli': 'file:./tmp/nx/build/packages/cli',
+        '@nrwl/cypress': 'file:./tmp/nx/build/packages/cypress',
+        '@nrwl/eslint-plugin-nx':
+          'file:./tmp/nx/build/packages/eslint-plugin-nx',
+        '@nrwl/jest': 'file:./tmp/nx/build/packages/jest',
+        '@nrwl/linter': 'file:./tmp/nx/build/packages/linter',
+        '@nrwl/tao': 'file:./tmp/nx/build/packages/tao',
+        '@nrwl/workspace': 'file:./tmp/nx/build/packages/workspace',
+      },
+    };
+
+    writeFileSync(PKG_JSON_PATH, JSON.stringify(updatedPkgJson, null, 2));
+
+    execSync(`yarn install`, { stdio: 'ignore' });
+  });
+}
+
+tearDown();
+cloneNx();
+installNxDeps();
+buildNx();
+updatePackageJsonPointToDist();
